72e1c294fa9abbd598b95aa7f4bcf599de30a7f9
lakehalmaya <lak.maya22@gmail.com>
2024-05-05 21:47:47 +0100

    try webhook

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -4,7 +4,6 @@ export default function Sponsors() {
   return (
     <div>
      <h1 className='text-center mt-10 text-5xl text-purple-900'>Happy Coding</h1>
-     <h1 className='text-center mt-10 text-5xl text-purple-900'>Happy Coding</h1>
     </div>
   )
 }
