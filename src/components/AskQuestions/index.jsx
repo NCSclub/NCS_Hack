da28c5b0b221bd081db5ac65e32f6d5c5f7b0ccb
lakehalmaya <lak.maya22@gmail.com>
2024-05-13 00:06:20 +0100

    changed cards bg & main bg

--- src/components/AskQuestions/index.jsx
+++ src/components/AskQuestions/index.jsx
@@ -3,46 +3,46 @@ import Vortex from "./vortex";
 const VortexDemo = () => {
   return (
     <div className="">
-      <div className="w-[365px] mx-auto my-6">
+      <div className="w-[365px] mx-auto mb-12">
         <h1 className="text-white text-center text-3xl font-bold">Frequently Asked <span className="text-[#3BC3EE]">Questions</span></h1>
       </div>
       <div className="flex">
-      <div className="w-72 mx-auto rounded-[60px] h-max overflow-hidden border-4 border-[#F9DAFB]/45">
-      <Vortex className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+      <div className="w-72 mx-auto rounded-[60px] h-96 overflow-hidden border-4 border-[#F9DAFB]/45">
+      <Vortex backgroundColor="transparent" className= "flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
           <div className="text-center">
-            <p className="text-[#3BC3EE] text-center text-3xl">01</p>
+            <p className="text-[#3BC3EE] text-center text-3xl font-semibold">01</p>
             <h4 className="text-[#F9DAFB] py-4 font-bold text-xl">NCS hack</h4>
-            <h4 className="text-white">1. What is Ncs Hack about?</h4>
+            <h4 className="text-white py-4">1. What is Ncs Hack about?</h4>
             <p className="text-[#F9DAFB]">TanahAir offers a service for creating a website design, illustration, icon set, website development, animation and digital marketing.</p>
           </div>
         </Vortex>
       </div>
-      <div className="w-72 mx-auto rounded-[60px] h-max overflow-hidden border-4 border-[#F9DAFB]/45">
-      <Vortex className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+      <div className="w-72 mx-auto rounded-[60px] h-96 overflow-hidden border-4 border-[#F9DAFB]/45">
+      <Vortex backgroundColor="transparent" className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
           <div className="text-center">
-          <p className="text-white text-center text-3xl">02</p>
+          <p className="text-[#3BC3EE] text-center text-3xl font-semibold">02</p>
             <h4 className="text-[#F9DAFB] py-4 text-xl">NCS hack</h4>
-            <h4 className="text-white">1. What is Ncs Hack about?</h4>
+            <h4 className="text-white py-4">1. What is Ncs Hack about?</h4>
             <p className="text-[#F9DAFB]">TanahAir offers a service for creating a website design, illustration, icon set, website development, animation and digital marketing.</p>
           </div>
         </Vortex>
       </div>
-      <div className="w-72 mx-auto rounded-[60px] h-max overflow-hidden border-4 border-[#F9DAFB]/45">
-      <Vortex className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+      <div className="w-72 mx-auto rounded-[60px] h-96 overflow-hidden border-4 border-[#F9DAFB]/45">
+      <Vortex backgroundColor="transparent" className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
           <div className="text-center">
-          <p className="text-white text-center text-3xl">03</p>
+          <p className="text-[#3BC3EE] text-center text-3xl font-semibold">03</p>
             <h4 className="text-[#F9DAFB] py-4 text-xl">NCS hack</h4>
-            <h4 className="text-white">1. What is Ncs Hack about?</h4>
+            <h4 className="text-white py-4">1. What is Ncs Hack about?</h4>
             <p className="text-[#F9DAFB]">TanahAir offers a service for creating a website design, illustration, icon set, website development, animation and digital marketing.</p>
           </div>
         </Vortex>
       </div>
-      <div className="w-72 mx-auto rounded-[60px] h-max overflow-hidden border-4 border-[#F9DAFB]/45">
-      <Vortex className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
+      <div className="w-72 mx-auto rounded-[60px] h-96 overflow-hidden border-4 border-[#F9DAFB]/45">
+      <Vortex backgroundColor="transparent" className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
           <div className="text-center">
-          <p className="text-white text-center text-3xl">04</p>
+          <p className="text-[#3BC3EE] text-center text-3xl font-semibold">04</p>
             <h4 className="text-[#F9DAFB] py-4 text-xl">NCS hack</h4>
-            <h4 className="text-white">1. What is Ncs Hack about?</h4>
+            <h4 className="text-white py-4">1. What is Ncs Hack about?</h4>
             <p className="text-[#F9DAFB]">TanahAir offers a service for creating a website design, illustration, icon set, website development, animation and digital marketing.</p>
           </div>
         </Vortex>
