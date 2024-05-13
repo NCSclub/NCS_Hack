31c76985494b5abc9994d780bbb058d6d3744fc4
Anis-Hemici <anishemici8@gmail.com>
2024-05-13 01:30:03 +0100

    adding needed config for the spotlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,12 +8,38 @@ module.exports = {
   ],
   theme: {
     extend: {
+      
+      
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+
+
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+          "bgtimer":"url('/images/Hero/Frame1.png')"
+        },
+
+
     },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 };
+
