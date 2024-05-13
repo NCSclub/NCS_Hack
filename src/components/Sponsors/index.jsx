ee2e34c60a0cbc50f737f6ad3b0b4ebac3322f1d
Anis-Hemici <anishemici8@gmail.com>
2024-05-13 01:30:54 +0100

    remove " happy coding h2 " :/

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 export default function Sponsors() {
   return (
     <div>
-     <h1 className='text-center mt-10 text-5xl text-purple-900'>Happy Coding</h1>
-     <h1 className='text-center mt-10 text-5xl text-purple-900'>Happy Coding</h1>
     </div>
   )
 }
