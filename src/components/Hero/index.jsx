62defda863aba78fca8825803041c6ac018263fb
Anis-Hemici <anishemici8@gmail.com>
2024-05-15 13:32:56 +0100

    change the button sryle

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import CountdownTimer from "./counterdown";
 import { HoverBorderGradientDemo } from "./components/ui/hover-border-gradientComp";
 import { SpotlightPreview } from "./components/ui/SpotlightComp";
+import styles from  "@/styles/hero.module.css"
+import Link from "next/link";
+
 export default function Hero() {
   return (
     <section id="hero ">
@@ -17,14 +20,15 @@ export default function Hero() {
         />
       </div>
 
-      <div className="timer&location flex md:flex-row flex-col-reverse  items-center justify-center gap-4 mt-7  lg:ml-[120px]">
+      <div className="timer&location flex md:flex-row flex-col-reverse  
+      items-center justify-center gap-4 mt-7  lg:ml-[120px]">
         <div className="flex flex-col gap-4">
           <div className="flex flex-row items-center gap-2">
             <Image
               className="w-[40px] "
               height={500}
               width={500}
-              src="/images/Hero/calender.png"
+              src="/images/Hero/cld.svg"
             />
             <h3 className="text-white">24-25-26 MAI</h3>
           </div>
@@ -34,11 +38,30 @@ export default function Hero() {
               className="w-[40px] "
               height={500}
               width={500}
-              src="/images/Hero/folowmap.png"
+              src="/images/Hero/followmap.svg"
             />
-            <h3 className="text-white max-w-[290px] ">
+           
+          
+
+           <Link
+            className="flex justify-center items-center gap-6 "
+            target="_blank"
+            href="https://maps.app.goo.gl/9Aeizen7VEjrkLD6A"
+          >
+            <h3 className="text-white max-w-[290px] hover:text-blue-500 duration-75 ">
               NUMIDI INSTITUE OF TEACHNOLOGY AlGER-RAHMANIA
             </h3>
+          </Link>
+
+
+
+
+
+
+
+
+
+            
             <h4 className="text-white"></h4>
           </div>
         </div>
@@ -52,9 +75,27 @@ export default function Hero() {
         </div>
       </div>
 
-      <div className="flex justify-center items-center  ">
-        <HoverBorderGradientDemo />
+      <div className="flex justify-center items-center  mt-10  ">
+     
+
+
+      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px] w-[200px] md:w-[300px] md:h-[60px] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-50 transition-transform duration-500 transform hover:translate-y-[-5px] active:translate-y-[-5px]">
+     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#2444D5_0%,#24A6D5_50%,#2444D5_100%)]" />
+    <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-md md:text-[22px] font-medium text-white backdrop-blur-3xl">
+    REGISTER NOW
+  </span>
+</button>
+
       </div>
+    
+    <div className=" flex flex-col gap-4 justify-center items-center mt-8 ">
+     <h3 className='text-white capitalize'>scroll to explor </h3>
+      <Image className="animate-bounce duration-75 w-[50px]"  width={100} height={100} src="images\Hero\scroll-down.svg"/>
+    </div>
+  
     </section>
   );
 }
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      
-      
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -24,22 +23,22 @@ module.exports = {
             transform: "translate(-50%,-40%) scale(1)",
           },
         },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
       },
-
-
-
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "bgtimer":"url('/images/Hero/Frame1.png')"
-        },
-
-
+        "bgtimer": "url('/images/Hero/Frame1.png')",
+      },
     },
   },
-  plugins: [
-
-  ],
+  plugins: [],
 };
-
