272e190159130e4c6f3717008789c33f4feea2a8
yasuogg <sohaibben52@gmail.com>
2024-05-16 21:31:51 +0100

    spotlight problem

--- src/components/Hero/components/ui/SpotlightComp.jsx
+++ src/components/Hero/components/ui/SpotlightComp.jsx
@@ -4,10 +4,10 @@ import { SparklesPreview } from "./sparklescomp";
 export function SpotlightPreview() {
   return (
     <div className=" w-full  rounded-md flex md:items-center md:justify-center  antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight
+      {/* <Spotlight
         className="-top-40  md:left-60 lg:left-[590px]  md:-top-20"
         fill="white"
-      />
+      /> */}
             <SparklesPreview/>
 
     </div>
