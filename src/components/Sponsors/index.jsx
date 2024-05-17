87f4ecc45be32fff8fd557ddc649be5ad4d31628
yasuogg <sohaibben52@gmail.com>
2024-05-17 14:21:30 +0100

    add key art to the map

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -8,24 +8,35 @@ export default function Sponsors() {
     { name: 'test', path: '/images/Sponsors/ncshacklogo_c1.svg' },
     { name: 'test', path: '/images/Sponsors/ncshacklogo_c1.svg' },
     { name: 'test', path: '/images/Sponsors/ncshacklogo_c1.svg' },
-
   ]
+
   return (
     <section className='py-16'>
-      <h1 className='text-center text-5xl md:text-7xl font-semibold'><span className=' text-[#0EC9E3]'>our</span> sponsors</h1>
-      <p className='text-center uppercase font-light mt-4'>we have beeN working with some fortune + 500 client </p>
+      <h1 className='text-center text-5xl md:text-7xl font-semibold'>
+        <span className='text-[#0EC9E3]'>our</span> sponsors
+      </h1>
+      <p className='text-center uppercase font-light mt-4'>
+        we have beeN working with some fortune + 500 client
+      </p>
       
       <ul className='flex flex-col md:flex-row justify-center items-center'>
-        {sponsors.map((logo, key) => (
-
-          <li>
-
-            <Link  href='#'><img key={key} alt="" loading="lazy" width="300" height="400" decoding="async" data-nimg="1" class="  hover:brightness-100 object-contain  hover:grayscale-0 transition-all duration-300 ease-in-out  brightness-75 grayscale" src={logo.path} /></Link>
+        {sponsors.map((logo, index) => (
+          <li key={index}>
+            <Link href='#'>
+              <img 
+                alt={logo.name} 
+                loading="lazy" 
+                width="300" 
+                height="400" 
+                decoding="async" 
+                data-nimg="1" 
+                className="hover:brightness-100 object-contain hover:grayscale-0 transition-all duration-300 ease-in-out brightness-75 grayscale" 
+                src={logo.path} 
+              />
+            </Link>
           </li>
-
         ))}
       </ul>
     </section>
-
   )
 }
